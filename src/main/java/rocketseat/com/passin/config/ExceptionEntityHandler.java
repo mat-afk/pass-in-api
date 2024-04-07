fa9851eeef8005601bef5ab3f60f84f469d2eddf
Mateus <mateusc.ramos@outlook.com>
2024-04-06 22:47:53 -0300

    feat: error handling

--- src/main/java/rocketseat/com/passin/config/ExceptionEntityHandler.java
+++ src/main/java/rocketseat/com/passin/config/ExceptionEntityHandler.java
@@ -1,15 +1,45 @@
 package rocketseat.com.passin.config;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import rocketseat.com.passin.domain.attendee.exceptions.AttendeeAlreadyRegisteredException;
+import rocketseat.com.passin.domain.attendee.exceptions.AttendeeNotFoundException;
+import rocketseat.com.passin.domain.checkin.exceptions.CheckInAlreadyExistsException;
+import rocketseat.com.passin.domain.event.exceptions.EventIsFullException;
 import rocketseat.com.passin.domain.event.exceptions.EventNotFoundException;
+import rocketseat.com.passin.dto.general.ErrorResponseDTO;
 
 @ControllerAdvice
 public class ExceptionEntityHandler {
 
     @ExceptionHandler(EventNotFoundException.class)
-    public ResponseEntity handleEventNotFound(EventNotFoundException exception) {
+    public ResponseEntity<String> handleEventNotFound(EventNotFoundException exception) {
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(EventIsFullException.class)
+    public ResponseEntity<ErrorResponseDTO> handleEventIsFullException(EventIsFullException exception) {
+        return ResponseEntity.badRequest().body(new ErrorResponseDTO(exception.getMessage()));
+    }
+
+    @ExceptionHandler(AttendeeNotFoundException.class)
+    public ResponseEntity<String> handleAttendeeNotFound(AttendeeNotFoundException exception) {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(AttendeeAlreadyRegisteredException.class)
+    public ResponseEntity<ErrorResponseDTO> handleAttendeeAlreadyRegisteredException(
+            AttendeeAlreadyRegisteredException exception
+    ) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(new ErrorResponseDTO(exception.getMessage()));
+    }
+
+    @ExceptionHandler(CheckInAlreadyExistsException.class)
+    public ResponseEntity<ErrorResponseDTO> handleCheckInAlreadyExistsException(
+            CheckInAlreadyExistsException exception
+    ) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(new ErrorResponseDTO(exception.getMessage()));
     }
 }
